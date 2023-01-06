2555fd49f241486c8bd7b6ecad56ec0df7196e88
DASitby <davidsooley551@gmail.com>
2023-01-06 02:15:26 +0000

    add route handler for u/:id to go directly to longURL

--- express_server.js
+++ express_server.js
@@ -36,6 +36,11 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
